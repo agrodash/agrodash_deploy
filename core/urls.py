b9e76c5ce3fff09d6f176d7af6e60f251394e8bf
agrodash <agrodash303@gmail.com>
2026-01-15 02:54:26 -0300

    indices

--- core/urls.py
+++ core/urls.py
@@ -37,6 +37,5 @@ urlpatterns = [
     path('', usuarios_views.home_view, name='home'),
 ]
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
