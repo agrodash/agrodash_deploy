811992743555a369120bfda2a59922914125de38
agrodash <agrodash303@gmail.com>
2026-01-15 03:00:02 -0300

    indices

--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from usuarios import views as usuarios_views
 
 urlpatterns = [
@@ -37,5 +38,11 @@ urlpatterns = [
     path('', usuarios_views.home_view, name='home'),
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Servir arquivos de media em desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Servir arquivos de media em produção
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
